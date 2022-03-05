f87cb171b8493162998da7d9edad9db0f2a5f25d
Bruno Rodrigues <github@brunodsr.com>
2022-03-05 18:22:11 -0300

    fix: logout button size

--- components/AccountDropdown.vue
+++ components/AccountDropdown.vue
@@ -14,7 +14,7 @@
           <i class="mdi mdi-lock-reset mr-3 h-5 w-5 " aria-hidden="true" />
           Trocar senha
         </a>
-        <button @click="logout" class="block px-4 py-2 text-gray-200 hover:bg-green-500 hover:text-white">
+        <button @click="logout" class="block w-full text-left px-4 py-2 text-gray-200 hover:bg-green-500 hover:text-white">
           <i class="mdi mdi-logout mr-3 h-5 w-5 " aria-hidden="true" />
           Sair
         </button>
