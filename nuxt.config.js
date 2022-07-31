2f09f11b72c82f76a3d377b2fb2725a713b5ddd3
Bruno Rodrigues <github@brunodsr.com>
2022-07-31 18:08:36 -0300

    Fav icon e chat facebook

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,8 +39,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "~/plugins/vee-validate.js",
+    '~/plugins/vee-validate.js',
     '~/plugins/v-mask.js',
+    '~/plugins/v-fb-customer-chat.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
