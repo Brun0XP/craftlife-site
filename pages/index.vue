2b58f2ab79a0a7aa64408dd0b94c7add738aecf9
Bruno Rodrigues <github@brunodsr.com>
2022-09-25 15:18:46 -0300

    Corrige responsividade home screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <Container>
-    <div class="col-span-8">
+    <div class="col-span-12 xl:col-span-8">
       <div class="bg-gray-800 rounded-lg mb-4 p-4">
         <p class="text-xl text-center text-gray-200">Últimas novidades</p>
       </div>
@@ -9,7 +9,7 @@
 
       </div>
     </div>
-    <div class="col-span-4 flex justify-end">
+    <div class="col-span-12 xl:col-span-4 flex justify-end">
       <iframe src="https://discordapp.com/widget?id=94235856516153344&theme=dark" width="405" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
     </div>
   </Container>
