9d16d42bb731e3c537ce3e8ac0b1a050fff795f2
Bruno Rodrigues <github@brunodsr.com>
2022-09-25 13:28:43 -0300

    Facebook chat fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,5 @@
     <Header />
     <Nuxt />
     <Footer />
-
-    <div id="fb-root"></div>
-    <div id="fb-customer-chat" class="fb-customerchat" page_id="575494299163994" attribution="biz_inbox"></div>
   </div>
 </template>
